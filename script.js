94e14503a0594ed09e29bea1200b211f03086cb7
Jr Hernandez <jrhernande25@g.crookcountyschools.org>
2022-10-17 14:14:01 -0700

    Added somethings

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function multiplication(number1, number2) {
+    let equalToo = number1 * number2
+    console.log("When multiplying " + number1 + " multiply " + number2 + " is equal to " + equalToo )
+}
+
+function addition(number1, number2) {
+    let equalToo = number1 + number2
+    console.log("When adding " + number1 + " plus " + number2 + " is equal to " + equalToo )
+}
+
+multiplication(100, 200)
+addition(100, 200);
+
+let name = "Josh has no idea of what to do" ;
+
+console.log("Josh has no idea of what to do") ;
+
+document.write("<h1>Josh</h1>")
+
+alert('This computer has a virus')
+
+prompt("Do you think your computer has a virus?")
